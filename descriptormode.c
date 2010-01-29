c11386d12f639dea183433bcd13d1fe440c7016c
Matthias Wenzel <dect@mazzoo.de>
2010-01-30 00:40:37 +0100

    make base addresses 32bit

--- descriptormode.c
+++ descriptormode.c
@@ -21,17 +21,17 @@ struct flash_descriptor {
 
 	/* information from above (redundantly kept here) */
 	uint8_t  NR;   /* number of regions */
-	uint8_t  FRBA; /* */
+	uint32_t FRBA; /* */
 	uint8_t  NC;   /* number of components */
-	uint8_t  FCBA; /* */
+	uint32_t FCBA; /* */
 
 	uint8_t  ISL;
-	uint8_t  FISBA;
+	uint32_t FISBA;
 	uint8_t  NM;
-	uint8_t  FMBA;
+	uint32_t FMBA;
 
 	uint8_t  MSL;
-	uint8_t  FMSBA;
+	uint32_t FMSBA;
 } fd;
 
 void gather_FDBAR(void)
@@ -42,40 +42,42 @@ void gather_FDBAR(void)
 	fd.FLMAP2   = fm[3];
 
 	fd.NR    = (fd.FLMAP0 >> 24) & 0x07;
-	fd.FRBA  =  fd.FLMAP0 >> 16;
+	fd.FRBA  = (fd.FLMAP0 >> 12) & 0x00000ff0;
 	fd.NC    = (fd.FLMAP0 >>  8) & 0x03;
-	fd.FCBA  =  fd.FLMAP0;
+	fd.FCBA  = (fd.FLMAP0 <<  4) & 0x00000ff0;
 
 	fd.ISL   =  fd.FLMAP1 >> 24;
-	fd.FISBA =  fd.FLMAP1 >> 16;
+	fd.FISBA = (fd.FLMAP1 >> 12) & 0x00000ff0;
 	fd.NM    = (fd.FLMAP1 >>  8) & 0x07;
-	fd.FMBA  =  fd.FLMAP1;
+	fd.FMBA  = (fd.FLMAP1 <<  4) & 0x00000ff0;
 
 	fd.MSL   =  fd.FLMAP2 >>  8;
-	fd.FMSBA =  fd.FLMAP2;
+	fd.FMSBA = (fd.FLMAP2 <<  4) & 0x00000ff0;
 }
 
 void dump_FDBAR(void)
 {
+	printf("\n");
 	printf("=== FDBAR ===\n");
 	printf("FLVALSIG 0x%8.8x\n", fd.FLVALSIG);
 	printf("FLMAP0   0x%8.8x\n", fd.FLMAP0  );
 	printf("FLMAP1   0x%8.8x\n", fd.FLMAP1  );
 	printf("FLMAP2   0x%8.8x\n", fd.FLMAP2  );
-
+	printf("\n");
 	printf("--- FDBAR details ---\n");
-	printf("0x%2.2x  NR    Number Of Regions\n", fd.NR   );
-	printf("0x%2.2x  FRBA  Flash Region Base Address\n", fd.FRBA );
-	printf("0x%2.2x  NC    Number Of Components\n", fd.NC   );
-	printf("0x%2.2x  FCBA  Flash Component Base Address\n", fd.FCBA );
-
-	printf("0x%2.2x  ISL   ICH Strap Length\n", fd.ISL  );
-	printf("0x%2.2x  FISBA Flash ICH Strap Base Address\n", fd.FISBA);
-	printf("0x%2.2x  NM    Number Of Masters\n", fd.NM   );
-	printf("0x%2.2x  FMBA  Flash Master Base Address\n", fd.FMBA );
-
-	printf("0x%2.2x  MSL   MCH Strap Length\n", fd.MSL  );
-	printf("0x%2.2x  FMSBA Flash MCH Strap Base Address\n", fd.FMSBA);
+	printf("0x%2.2x        NR    Number Of Regions\n", fd.NR   );
+	printf("0x%8.8x  FRBA  Flash Region Base Address\n", fd.FRBA );
+	printf("0x%2.2x        NC    Number Of Components\n", fd.NC   );
+	printf("0x%8.8x  FCBA  Flash Component Base Address\n", fd.FCBA );
+	printf("\n");
+	printf("0x%2.2x        ISL   ICH Strap Length\n", fd.ISL  );
+	printf("0x%8.8x  FISBA Flash ICH Strap Base Address\n", fd.FISBA);
+	printf("0x%2.2x        NM    Number Of Masters\n", fd.NM   );
+	printf("0x%8.8x  FMBA  Flash Master Base Address\n", fd.FMBA );
+	printf("\n");
+	printf("0x%2.2x        MSL   MCH Strap Length\n", fd.MSL  );
+	printf("0x%8.8x  FMSBA Flash MCH Strap Base Address\n", fd.FMSBA);
+	printf("\n");
 }
 
 void usage(void)
