52803ed8fe1e57448e821bf2466eba1b5d77b84d
Matthias Wenzel <dect@mazzoo.de>
2010-01-29 07:28:45 +0100

    detect descriptor mode

--- descriptormode.c
+++ descriptormode.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-int f; /* file descriptor to flash file */
+int f;         /* file descriptor to flash file */
+int fs;        /* file size */
+uint32_t * fm; /* mmap'd file */
+
+#define DESCRIPTOR_MODE_MAGIC 0x0ff0a55a
+
+void usage(void)
+{
+	printf("no.\n");
+	exit(1);
+}
 
 int main(int argc, char ** argv)
 {
+	if (argc < 2) usage();
+	f = open(argv[1], O_RDONLY);
+	if (f < 0) usage();
+
+	fs = lseek(f, 0, SEEK_END);
+	if (fs < 0) usage();
+
+	fm = mmap(NULL, fs, PROT_READ, MAP_PRIVATE, f, 0);
+	if (fm == (void *) -1) usage();
+
+	if (fm[0] != DESCRIPTOR_MODE_MAGIC)
+	{
+		printf("not in descriptor mode\n");
+		exit(0);
+	}
+
+
 	return 0;
 }
+
